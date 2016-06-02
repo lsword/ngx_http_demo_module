848b9202960f26dec14bbb79a42c60a8e1f87c49
lsword <lsword@github.io>
2016-06-02 14:26:38 +0800

    add demo for upstream

--- ngx_http_demo_module.c
+++ ngx_http_demo_module.c
@@ -5,6 +5,7 @@
 static char *ngx_http_demo(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_demo_handler(ngx_http_request_t *r);
 static char* ngx_http_demo_arg_strscript(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static char* ngx_http_demo_arg_compilecomplexvalue(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static void* ngx_http_demo_create_loc_conf(ngx_conf_t *cf);
 
@@ -44,6 +45,11 @@ typedef struct {
     ngx_array_t *arg_strscript_lengths;
     ngx_array_t *arg_strscript_values;
 	ngx_str_t arg_strscript;
+
+    ngx_http_complex_value_t *arg_compilecomplexvalue;
+    ngx_str_t arg_compilecomplexvalue_str;
+
+    ngx_http_upstream_conf_t upstreamconf;
 
 } ngx_http_demo_loc_conf_t;
 
@@ -146,6 +152,21 @@ static ngx_command_t ngx_http_demo_commands[] = {
     0,
     NULL },
 
+    { ngx_string("demo_arg_compilecomplexvalue"),
+    NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+    ngx_http_demo_arg_compilecomplexvalue,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    0,
+    NULL },
+
+    { ngx_string("demo_ups_connect_timeout"),
+    NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+    ngx_conf_set_msec_slot,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    offsetof(ngx_http_demo_loc_conf_t, upstreamconf.connect_timeout),
+    //offsetof(ngx_http_demo_loc_conf_t, arg_msec),
+    NULL },
+
     ngx_null_command
 };
 
@@ -219,9 +240,18 @@ static ngx_int_t ngx_http_demo_handler(ngx_http_request_t *r) {
 		return NGX_HTTP_INTERNAL_SERVER_ERROR;
 	}
 
+    ngx_http_complex_value(r, democf->arg_compilecomplexvalue, &democf->arg_compilecomplexvalue_str);
+
+    if (ngx_http_upstream_create(r) != NGX_OK) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "ngx_http_upstream_create() failed");
+        return NGX_ERROR;
+    }
+    //r->upstream->conf->connect_timeout = democf->upstream_connect_timeout;
+    r->upstream->conf = &democf->upstreamconf;
+
     u_char buffer[1000];
     ngx_memset(buffer, 0, sizeof(buffer));
-    ngx_sprintf(buffer, 
+    ngx_sprintf(buffer,
             "arg_flag: %d\n"
 			"arg_str: %V\n"
             "arg_num: %d\n"
@@ -234,13 +264,15 @@ static ngx_int_t ngx_http_demo_handler(ngx_http_request_t *r) {
             "arg_bufs:\n\tnum: %d\n\tsize: %d\n"
             "arg_enum: %d\n"
             "arg_bitmask: %d\n"
-            "arg_strscript: %V\n",
-            democf->arg_flag, &democf->arg_str, democf->arg_num, 
+            "arg_strscript: %V\n"
+            "arg_compilecomplexvalue: %V\n",
+            democf->arg_flag, &democf->arg_str, democf->arg_num,
             (char*)buffer_strarray, (char*)buffer_keyval, democf->arg_size,
             democf->arg_off, democf->arg_msec, democf->arg_sec,
             democf->arg_bufs.num, democf->arg_bufs.size, democf->arg_enum,
 			democf->arg_bitmask,
-            &democf->arg_strscript);
+            &democf->arg_strscript,
+            &democf->arg_compilecomplexvalue_str);
     ngx_str_t response = ngx_string(buffer);
 
     r->headers_out.status = NGX_HTTP_OK;
@@ -288,9 +320,36 @@ static void* ngx_http_demo_create_loc_conf(ngx_conf_t *cf) {
     conf->arg_msec = NGX_CONF_UNSET;
     conf->arg_sec = NGX_CONF_UNSET;
     conf->arg_enum = NGX_CONF_UNSET_UINT;
+    conf->arg_msec = NGX_CONF_UNSET;
+
+    conf->upstreamconf.connect_timeout = 5000;
+    conf->upstreamconf.send_timeout = 5000;
+    conf->upstreamconf.read_timeout = 5000;
+    conf->upstreamconf.store_access = 0600;
+    conf->upstreamconf.buffering = 0;
+    conf->upstreamconf.bufs.num = 8;
+    conf->upstreamconf.bufs.size = ngx_pagesize;
+    conf->upstreamconf.buffer_size = ngx_pagesize;
+    conf->upstreamconf.busy_buffers_size = 2*ngx_pagesize;
+    conf->upstreamconf.temp_file_write_size = 2*ngx_pagesize;
+    conf->upstreamconf.max_temp_file_size = 1024*1024*1024;
+    conf->upstreamconf.hide_headers = NGX_CONF_UNSET_PTR;
+    conf->upstreamconf.pass_headers = NGX_CONF_UNSET_PTR;
 
     return conf;
 }
+
+static ngx_str_t  ngx_http_proxy_hide_headers[] = {
+    ngx_string("Date"),
+    ngx_string("Server"),
+    ngx_string("X-Pad"),
+    ngx_string("X-Accel-Expires"),
+    ngx_string("X-Accel-Redirect"),
+    ngx_string("X-Accel-Limit-Rate"),
+    ngx_string("X-Accel-Buffering"),
+    ngx_string("X-Accel-Charset"),
+    ngx_null_string
+};
 
 static char* ngx_http_demo_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child) {
     ngx_http_demo_loc_conf_t *prev = parent;
@@ -310,6 +369,16 @@ static char* ngx_http_demo_merge_loc_conf(ngx_conf_t *cf, void *parent, void *ch
     ngx_conf_merge_bitmask_value(conf->arg_bitmask, prev->arg_bitmask,
                               (NGX_CONF_BITMASK_SET | NGX_http_demo_BIT1));
     ngx_conf_merge_str_value(conf->arg_str, prev->arg_str, "default");
+
+    ngx_conf_merge_msec_value(conf->upstreamconf.connect_timeout, prev->upstreamconf.connect_timeout, 5000);
+
+    ngx_hash_init_t hash;
+    hash.max_size = 100;
+    hash.bucket_size = 1024;
+    hash.name = "proxy_headers_hash";
+    if (ngx_http_upstream_hide_headers_hash(cf, &conf->upstreamconf, &prev->upstreamconf, ngx_http_proxy_hide_headers, &hash) != NGX_OK )
+        return NGX_CONF_ERROR;
+
 
     return NGX_CONF_OK;
 }
@@ -360,3 +429,20 @@ static ngx_int_t ngx_http_demo_arg_strscript_eval(ngx_http_request_t *r, ngx_htt
     return NGX_OK;
 }
 
+static char* ngx_http_demo_arg_compilecomplexvalue(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+    ngx_http_demo_loc_conf_t *dlcf = conf;
+    ngx_http_compile_complex_value_t   ccv;
+    ngx_str_t *value;
+
+    value = cf->args->elts;
+    ngx_memzero(&ccv, sizeof(ngx_http_compile_complex_value_t));
+    ccv.cf = cf;
+    ccv.value = &value[1];
+    ccv.complex_value = ngx_palloc(cf->pool, sizeof(ngx_http_complex_value_t));
+    ccv.zero = 1;
+    //ccv.conf_prefix = 1;
+    ccv.root_prefix = 1;
+    ngx_http_compile_complex_value(&ccv);
+    dlcf->arg_compilecomplexvalue = ccv.complex_value;
+    return NGX_CONF_OK;
+}
