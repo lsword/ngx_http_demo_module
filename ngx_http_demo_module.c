10140586d7f7b18e659d54ce87801f57fd27f92c
lsword <lsword@github.io>
2016-06-03 15:14:42 +0800

    update upstream demo

--- ngx_http_demo_module.c
+++ ngx_http_demo_module.c
@@ -7,6 +7,12 @@ static ngx_int_t ngx_http_demo_handler(ngx_http_request_t *r);
 static char* ngx_http_demo_arg_strscript(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static char* ngx_http_demo_arg_compilecomplexvalue(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
+static char *ngx_http_demo_upstream(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_http_demo_upstream_handler(ngx_http_request_t *r);
+static ngx_int_t demo_upstream_create_request(ngx_http_request_t *r);
+static ngx_int_t demo_upstream_process_status_line(ngx_http_request_t *r);
+static void demo_upstream_finalize_request(ngx_http_request_t *r, ngx_int_t rc);
+
 static void* ngx_http_demo_create_loc_conf(ngx_conf_t *cf);
 
 static char* ngx_http_demo_merge_loc_conf(ngx_conf_t *cf,
@@ -27,6 +33,10 @@ static ngx_conf_bitmask_t  ngx_http_demo_bitmask[] = {
     { ngx_string("bit3"), NGX_http_demo_BIT3 },
     { ngx_null_string, 0 }
 };
+
+typedef struct {
+    ngx_http_status_t status;
+} ngx_http_demo_ctx_t;
 
 typedef struct {
     ngx_flag_t arg_flag;
@@ -57,6 +67,13 @@ static ngx_command_t ngx_http_demo_commands[] = {
     { ngx_string("demo"),
     NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
     ngx_http_demo,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    0,
+    NULL },
+
+    { ngx_string("demo_upstream"),
+    NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+    ngx_http_demo_upstream,
     NGX_HTTP_LOC_CONF_OFFSET,
     0,
     NULL },
@@ -242,13 +259,6 @@ static ngx_int_t ngx_http_demo_handler(ngx_http_request_t *r) {
 
     ngx_http_complex_value(r, democf->arg_compilecomplexvalue, &democf->arg_compilecomplexvalue_str);
 
-    if (ngx_http_upstream_create(r) != NGX_OK) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "ngx_http_upstream_create() failed");
-        return NGX_ERROR;
-    }
-    //r->upstream->conf->connect_timeout = democf->upstream_connect_timeout;
-    r->upstream->conf = &democf->upstreamconf;
-
     u_char buffer[1000];
     ngx_memset(buffer, 0, sizeof(buffer));
     ngx_sprintf(buffer,
@@ -301,6 +311,65 @@ static ngx_int_t ngx_http_demo_handler(ngx_http_request_t *r) {
     out.next = NULL;
 
     return ngx_http_output_filter(r, &out);
+}
+
+static char *ngx_http_demo_upstream(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+    ngx_http_core_loc_conf_t *clcf;
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_demo_upstream_handler;
+    return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_http_demo_upstream_handler(ngx_http_request_t *r) {
+    ngx_http_demo_ctx_t *democtx = ngx_http_get_module_ctx(r, ngx_http_demo_module);
+    if (democtx == NULL) {
+        democtx = ngx_palloc(r->pool, sizeof(ngx_http_demo_ctx_t));
+        if (democtx == NULL)
+            return NGX_ERROR;
+        ngx_http_set_ctx(r, democtx, ngx_http_demo_module);
+    }
+
+    ngx_http_demo_loc_conf_t *democf;
+    democf = ngx_http_get_module_loc_conf(r, ngx_http_demo_module);
+    ngx_http_upstream_t *u = r->upstream;
+    u->conf = &democf->upstreamconf;
+    u->buffering = democf->upstreamconf.buffering;
+    u->resolved = (ngx_http_upstream_resolved_t*)ngx_pcalloc(r->pool, sizeof(ngx_http_upstream_resolved_t));
+    if (u->resolved == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                "ngx_pcalloc resolved error. %s.", strerror(errno));
+        return NGX_ERROR;
+    }
+    static struct sockaddr_in backendSockAddr;
+    struct hostent *pHost = gethostbyname((char*)"www.weather.com.cn");
+    if (pHost == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "gethostbyname fail. %s", strerror(errno));
+        return NGX_ERROR;
+    }
+    backendSockAddr.sin_family = AF_INET;
+    backendSockAddr.sin_port = htons((in_port_t)80);
+    char *pDmsIP = inet_ntoa(*(struct in_addr*)(pHost->h_addr_list[0]));
+    backendSockAddr.sin_addr.s_addr = inet_addr(pDmsIP);
+    //democtx->backendServer.data = (u_char*)pDmsIP;
+    //democtx->backendServer.len = strlen(pDmsIP);
+
+    u->resolved->sockaddr = (struct sockaddr *)&backendSockAddr;
+    u->resolved->socklen = sizeof(struct sockaddr_in);
+    u->resolved->naddrs = 1;
+
+    u->create_request = demo_upstream_create_request;
+    u->process_header = demo_upstream_process_status_line;
+    u->finalize_request = demo_upstream_finalize_request;
+
+
+    if (ngx_http_upstream_create(r) != NGX_OK) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "ngx_http_upstream_create() failed");
+        return NGX_ERROR;
+    }
+
+    r->main->count++;
+    ngx_http_upstream_init(r);
+    return NGX_DONE;
 }
 
 static void* ngx_http_demo_create_loc_conf(ngx_conf_t *cf) {
@@ -445,4 +514,80 @@ static char* ngx_http_demo_arg_compilecomplexvalue(ngx_conf_t *cf, ngx_command_t
     ngx_http_compile_complex_value(&ccv);
     dlcf->arg_compilecomplexvalue = ccv.complex_value;
     return NGX_CONF_OK;
+}
+
+static ngx_int_t demo_upstream_create_request(ngx_http_request_t *r) {
+    //static ngx_str_t backendQueryLine = ngx_string("http://www.weather.com.cn/data/sk/101010100.html");
+    static ngx_str_t backendQueryLine = ngx_string("GET /data/sk/101010100.html");
+    //ngx_int_t queryLineLen = backendQueryLine.len + r->args.len - 2;
+    ngx_int_t queryLineLen = backendQueryLine.len-1;
+    ngx_buf_t* b = ngx_create_temp_buf(r->pool, queryLineLen);
+    if (b==NULL) {
+        return NGX_ERROR;
+    }
+    b->last = b->pos + queryLineLen;
+    ngx_snprintf(b->pos, queryLineLen, (char*)backendQueryLine.data);
+    r->upstream->request_bufs = ngx_alloc_chain_link(r->pool);
+    if (r->upstream->request_bufs == NULL) {
+        return NGX_ERROR;
+    }
+
+    r->upstream->request_bufs->buf = b;
+    r->upstream->request_bufs->next = NULL;
+
+    r->upstream->request_sent = 0;
+    r->upstream->header_sent = 0;
+
+    r->header_hash = 1;
+
+
+    return NGX_OK;
+}
+
+static ngx_int_t demo_upstream_process_header(ngx_http_request_t *r) {
+    return NGX_OK;
+}
+
+static ngx_int_t demo_upstream_process_status_line(ngx_http_request_t *r) {
+    size_t len;
+    ngx_int_t rc;
+    ngx_http_upstream_t *u;
+
+    ngx_http_demo_ctx_t *democtx = ngx_http_get_module_ctx(r, ngx_http_demo_module);
+    if (democtx == NULL) {
+        return NGX_ERROR;
+    }
+    u = r->upstream;
+
+    rc = ngx_http_parse_status_line(r, &u->buffer, &democtx->status);
+    if (rc == NGX_AGAIN) {
+        return rc;
+    }
+    if (rc == NGX_ERROR) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "upstream sent no valid HTTP/1.0 header");
+    }
+    r->http_version = NGX_HTTP_VERSION_9;
+
+    u->state->status = NGX_HTTP_OK;
+    if (u->state) {
+        u->state->status = democtx->status.code;
+    }
+    u->headers_in.status_n = democtx->status.code;
+
+    len = democtx->status.end - democtx->status.start;
+    u->headers_in.status_line.len = len;
+
+    u->headers_in.status_line.data = ngx_pnalloc(r->pool, len);
+    if (u->headers_in.status_line.data == NULL) {
+        NGX_ERROR;
+    }
+
+    ngx_memcpy(u->headers_in.status_line.data, democtx->status.start, len);
+    u->process_header = demo_upstream_process_header;
+
+    return demo_upstream_process_header(r);
+}
+
+static void demo_upstream_finalize_request(ngx_http_request_t *r, ngx_int_t rc) {
+    ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "mytest_upstream_finalize_request");
 }
