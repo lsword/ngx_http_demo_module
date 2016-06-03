2f8b6d1b221508ebcc5fd32dd527bf39211dce94
lsword <lsword@github.io>
2016-06-03 17:57:52 +0800

    update upstream demo

--- ngx_http_demo_module.c
+++ ngx_http_demo_module.c
@@ -329,9 +329,15 @@ static ngx_int_t ngx_http_demo_upstream_handler(ngx_http_request_t *r) {
         ngx_http_set_ctx(r, democtx, ngx_http_demo_module);
     }
 
+    if (ngx_http_upstream_create(r) != NGX_OK) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "ngx_http_upstream_create() failed");
+        return NGX_ERROR;
+    }
+
     ngx_http_demo_loc_conf_t *democf;
     democf = ngx_http_get_module_loc_conf(r, ngx_http_demo_module);
     ngx_http_upstream_t *u = r->upstream;
+
     u->conf = &democf->upstreamconf;
     u->buffering = democf->upstreamconf.buffering;
     u->resolved = (ngx_http_upstream_resolved_t*)ngx_pcalloc(r->pool, sizeof(ngx_http_upstream_resolved_t));
@@ -341,31 +347,33 @@ static ngx_int_t ngx_http_demo_upstream_handler(ngx_http_request_t *r) {
         return NGX_ERROR;
     }
     static struct sockaddr_in backendSockAddr;
-    struct hostent *pHost = gethostbyname((char*)"www.weather.com.cn");
+    //struct hostent *pHost = gethostbyname((char*)"www.weather.com.cn");
+    struct hostent *pHost = gethostbyname((char*)"www.baidu.com");
     if (pHost == NULL) {
         ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "gethostbyname fail. %s", strerror(errno));
         return NGX_ERROR;
     }
     backendSockAddr.sin_family = AF_INET;
-    backendSockAddr.sin_port = htons((in_port_t)80);
-    char *pDmsIP = inet_ntoa(*(struct in_addr*)(pHost->h_addr_list[0]));
-    backendSockAddr.sin_addr.s_addr = inet_addr(pDmsIP);
+    backendSockAddr.sin_port = htons((in_port_t)8080);
+    //char *pDmsIP = inet_ntoa(*(struct in_addr*)(pHost->h_addr_list[0]));
+    //backendSockAddr.sin_addr.s_addr = inet_addr(pDmsIP);
+    //backendSockAddr.sin_addr.s_addr = inet_addr("61.4.185.201");
+    backendSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     //democtx->backendServer.data = (u_char*)pDmsIP;
     //democtx->backendServer.len = strlen(pDmsIP);
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, pHost->h_name);
 
     u->resolved->sockaddr = (struct sockaddr *)&backendSockAddr;
     u->resolved->socklen = sizeof(struct sockaddr_in);
+    ngx_str_t host = ngx_string("www.weather.com.cn");
+    u->resolved->host = host;
+    u->resolved->port = htons((in_port_t)8080);
     u->resolved->naddrs = 1;
 
     u->create_request = demo_upstream_create_request;
     u->process_header = demo_upstream_process_status_line;
     u->finalize_request = demo_upstream_finalize_request;
 
-
-    if (ngx_http_upstream_create(r) != NGX_OK) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "ngx_http_upstream_create() failed");
-        return NGX_ERROR;
-    }
 
     r->main->count++;
     ngx_http_upstream_init(r);
@@ -518,7 +526,8 @@ static char* ngx_http_demo_arg_compilecomplexvalue(ngx_conf_t *cf, ngx_command_t
 
 static ngx_int_t demo_upstream_create_request(ngx_http_request_t *r) {
     //static ngx_str_t backendQueryLine = ngx_string("http://www.weather.com.cn/data/sk/101010100.html");
-    static ngx_str_t backendQueryLine = ngx_string("GET /data/sk/101010100.html");
+    //static ngx_str_t backendQueryLine = ngx_string("GET /data/sk/101010100.html");
+    static ngx_str_t backendQueryLine = ngx_string("GET /index.html");
     //ngx_int_t queryLineLen = backendQueryLine.len + r->args.len - 2;
     ngx_int_t queryLineLen = backendQueryLine.len-1;
     ngx_buf_t* b = ngx_create_temp_buf(r->pool, queryLineLen);
@@ -545,6 +554,81 @@ static ngx_int_t demo_upstream_create_request(ngx_http_request_t *r) {
 }
 
 static ngx_int_t demo_upstream_process_header(ngx_http_request_t *r) {
+    ngx_int_t rc;
+    ngx_table_elt_t *h;
+    ngx_http_upstream_header_t *hh;
+    ngx_http_upstream_main_conf_t *umcf;
+
+    umcf = ngx_http_get_module_main_conf(r, ngx_http_upstream_module);
+    for (;;) {
+        rc = ngx_http_parse_header_line(r, &r->upstream->buffer, 1);
+        if (rc == NGX_OK) {
+            h = ngx_list_push(&r->upstream->headers_in.headers);
+            if (h==NULL) {
+                return NGX_ERROR;
+            }
+            h->hash = r->header_hash;
+            h->key.len = r->header_name_end - r->header_name_start;
+            h->value.len = r->header_end - r->header_start;
+
+            h->key.data = ngx_pnalloc(r->pool, h->key.len+1+h->value.len+1+h->key.len);
+            if (h->key.data == NULL) {
+                return NGX_ERROR;
+            }
+            h->value.data = h->key.data + h->key.len + 1;
+            h->lowcase_key = h->key.data + h->key.len + 1 + h->value.len + 1;
+            ngx_memcpy(h->key.data, r->header_name_start, h->key.len);
+            h->key.data[h->key.len] = '\0';
+            ngx_memcpy(h->value.data, r->header_start, h->value.len);
+            h->value.data[h->value.len] = '\0';
+
+            if (h->key.len == r->lowcase_index) {
+                ngx_memcpy(h->lowcase_key, r->lowcase_header, h->key.len);
+            } else {
+                ngx_strlow(h->lowcase_key, h->key.data, h->key.len);
+            }
+
+            hh = ngx_hash_find(&umcf->headers_in_hash, h->hash, h->lowcase_key, h->key.len);
+            if (hh && hh->handler(r, h, hh->offset) != NGX_OK) {
+                return NGX_ERROR;
+            }
+
+            continue;
+        }
+
+        if (rc == NGX_HTTP_PARSE_HEADER_DONE) {
+            if (r->upstream->headers_in.server == NULL) {
+                h = ngx_list_push(&r->upstream->headers_in.headers);
+                if (h == NULL) {
+                    return NGX_ERROR;
+                }
+                h->hash = ngx_hash(ngx_hash(ngx_hash(ngx_hash(ngx_hash('s','e'), 'r'), 'v'), 'e'), 'r');
+                ngx_str_set(&h->key, "Server");
+                ngx_str_null(&h->value);
+                h->lowcase_key = (u_char*)"server";
+            }
+
+            if (r->upstream->headers_in.date == NULL) {
+                h = ngx_list_push(&r->upstream->headers_in.headers);
+                if (h == NULL) {
+                    return NGX_ERROR;
+                }
+                h->hash = ngx_hash(ngx_hash(ngx_hash('d','a'), 't'), 'e');
+                ngx_str_set(&h->key, "Date");
+                ngx_str_null(&h->value);
+                h->lowcase_key = (u_char*)"date";
+            }
+            return NGX_OK;
+        }
+
+        if (rc == NGX_AGAIN) {
+            return NGX_AGAIN;
+        }
+
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "upstream sent invalid header");
+        return NGX_HTTP_UPSTREAM_INVALID_HEADER;
+    }
+
     return NGX_OK;
 }
 
